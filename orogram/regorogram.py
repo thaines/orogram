b4a4526d425a085e3460dd12190df93c03a6c979
Tom SF Haines <thaines@gmail.com>
2022-12-27 13:20:03 +0000

    Correcting numpy behaviour of pyximport.

--- orogram/regorogram.py
+++ orogram/regorogram.py
@@ -2,10 +2,10 @@
 
 import sys
 
-import pyximport
-pyximport.install()
-
 import numpy
+
+import pyximport
+pyximport.install(setup_args={'include_dirs': [numpy.get_include()]})
 
 try:
   from . import regorofun
